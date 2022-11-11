d2c14be564c56db6f987285ce5a34a57c6253896
giuseppecash <giuseppecash@hotmail.com>
2022-11-11 16:24:02 +0100

    Pages updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      black: "#292626",
+      blue: "#ECF2FF",
+      lightblue: "#5790FF",
+      darkblue: "#053083",
+      deepblue: "#03122F",
+    },
+    extend: {
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+    screens: {
+      lg: { max: "1024px" },
+      md: { min:"415px", max:"1023px" },
+      sm: { max: "414px" } 
+    }
   },
   plugins: [],
 }
